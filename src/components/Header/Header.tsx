7c85d3d71f53fed625c1e903ed71e88d3fe5da7b
Rinata Baygulova <rinulya0912@gmail.com>
2021-03-04 20:55:47 +0300

    feat: add Header

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,18 @@
+import styles from './Header.module.sass';
+
+export function Header() {
+  return (
+    <div className={styles.header}>
+      <div className={styles.content}>
+        <h1 className={styles.title}>
+          We Build <span className={styles.title_style_bold}>Brand</span>
+        </h1>
+        <p className={styles.subtitle}>
+          Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam
+          nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat
+          volutpat.
+        </p>
+      </div>
+    </div>
+  );
+}
